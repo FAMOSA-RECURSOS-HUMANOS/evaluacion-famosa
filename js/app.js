0380e37bd709c74684eb7815b9379b74e7925dd8
keytlinkaterineloayzavaldez-cloud <keytlinkaterineloayzavaldez@gmail.com>
2026-08-12 15:09:46 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -202,7 +202,7 @@ function armarPagina3(sufijo) {
 
     const listaGlobal = document.getElementById(`listaGlobal_${sufijo}`);
     listaGlobal.innerHTML = crearTarjetaEscala(
-      "Valoración Integral",
+      "Calificación Integral de Desempeño",
       "Considera el desempeño integral del colaborador durante el periodo evaluado.",
       `global_${sufijo}`,
       ETIQUETAS_ESCALA
